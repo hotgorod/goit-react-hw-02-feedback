997695fb421cc70f61a19d7fc4c1b39cc01f658b
hotgorod <hotgorod@gmail.com>
2023-09-19 09:48:59 +0100

    refactoring function onButtonClick

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+
 import Feedback from './Feedback'
 
 
-export class App extends Component {
-  
-  
+export const App = () => {
 
-
-  render() {
-    
-    return (<>
-      <h2 className='text'>Please leave feedback</h2>
+    return (      
       <Feedback />
       
-    </>);
+    );
   }
-}
+
 
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -7,57 +7,47 @@ class Feedback extends React.Component {
         neutral: 0,
         bad: 0
     }
-    statisticIncrementGood = () => {
+   
+    onButtonClick = (key) => {
         this.setState(prevState => {
             return {
-                good: prevState.good +1,
+                [key]: prevState[key] + 1,
             }
-          
         })
     }
-     statisticIncrementNeutral = () => {
-        this.setState(prevState => {
-            return {
-                neutral: prevState.neutral +1,
-            }
-          
-        })
-    }
-     statisticIncrementBad = () => {
-        this.setState(prevState => {
-            return {
-                bad: prevState.bad + 1,
-            }
-          
-        })
-    }
+
     countTotalFeedback = () => {
-        return this.state.good + this.state.neutral + this.state.bad
+        return Object.values(this.state).reduce((acc, state) => acc + state, 0);
         
     }
     countPositiveFeedbackPercentage = () => {
-
+        const totalFeedback = this.countTotalFeedback();
+        if (totalFeedback === 0) {
+            return 0; 
+        }
+        return Math.round((this.state.good * 100) / totalFeedback);
     }
+   
     render() {
-        const countTotalFeedback = Object.values(this.state).reduce((acc, state) => acc + state, 0); 
-        const countPositiveFeedbackPercentage = Math.round((this.state.good*100)/countTotalFeedback);
         return (<>
+            <h2 className='text'>Please leave feedback</h2>
                 <div className={css.buttonWrapper}>
-                    <button type="button" onClick={this.statisticIncrementGood} className={css.button55}>Good</button>
-                    <button type="button" onClick={this.statisticIncrementNeutral}className={css.button55}>Neutral</button>
-                    <button type="button" onClick={this.statisticIncrementBad}className={css.button55}>Bad</button>
+                    <button type="button" onClick={()=>this.onButtonClick('good')} className={css.button}>Good</button>
+                    <button type="button" onClick={()=>this.onButtonClick('neutral')} className={css.button}>Neutral</button>
+                    <button type="button" onClick={()=>this.onButtonClick('bad')}className={css.button}>Bad</button>
                 </div>
                 <p className={css.text}>Statistics</p>
                 <div>
                     <p className={css.textStatistics}>Good: {this.state.good}</p>
                     <p className={css.textStatistics}>Neutral: {this.state.neutral}</p>
                     <p className={css.textStatistics}>Bad: {this.state.bad}</p>
-                    <p className={css.textStatistics}>Total: {countTotalFeedback}</p>
-                    <p className={css.textStatistics}>Positive feedback: { countPositiveFeedbackPercentage}</p>
+                    <p className={css.textStatistics}>Total: {this.countTotalFeedback()}</p>
+                    <p className={css.textStatistics}>Positive feedback: {this.countPositiveFeedbackPercentage()} %</p>
                 </div>
             </>
         )
     }
+  
   }
 
 
